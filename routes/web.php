47dd70737303c29da3d28f81070ad20ec091b0df
okina3 <takosos@icloud.com>
2022-12-24 23:18:07 +0900

    【PHP/Laravel】05 ControllerとViewが連携できるようにしよう。 課題毎のコミット。

--- routes/web.php
+++ routes/web.php
@@ -22,20 +22,19 @@ use App\Http\Controllers\Admin\ProfileController;
 
 Route::get('/', function () {
     return view('welcome');
+});
 
-    Route::controller(NewsController::class)->prefix('admin')->group(function () {
-        Route::get('news/create', 'add');
-    });
+Route::controller(NewsController::class)->prefix('admin')->group(function () {
+    Route::get('news/create', 'add');
+});
 
-    //課題の3-----------------------------------------------------
-    Route::controller(AAAController::class)->group(function () {
-        Route::get('AAA/','bbb');
-    });
+//課題の3-----------------------------------------------------
+Route::controller(AAAController::class)->group(function () {
+    Route::get('AAA/', 'bbb');
+});
 
-    //課題の4-----------------------------------------------------
-    Route::controller(ProfileController::class)->prefix('admin')->group(function () {
-        Route::get('profile/create', 'add');
-        Route::get('profile/edit', 'edit');
-    });
-
+//課題の4-----------------------------------------------------
+Route::controller(ProfileController::class)->prefix('admin')->group(function () {
+    Route::get('profile/create', 'add');
+    Route::get('profile/edit', 'edit');
 });
