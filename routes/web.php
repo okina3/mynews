eea49db02601a49ed825529d598bc24bbbf2d8d5
okina3 <takosos@icloud.com>
2022-12-26 13:54:31 +0900

    課題前のコミット

--- routes/web.php
+++ routes/web.php
@@ -42,3 +42,8 @@ Route::controller(ProfileController::class)->prefix('admin')->group(function () 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::controller(NewsController::class)->prefix('admin')->group(function() {
+    Route::get('news/create', 'add')->middleware('auth');
+});
