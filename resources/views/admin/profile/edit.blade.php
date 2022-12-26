cc5f19e0f2a9fb738b7e71c364f29bcc30d00daf
okina3 <takosos@icloud.com>
2022-12-26 17:42:53 +0900

    【PHP/Laravel】08 ニュース投稿画面を作成しよう。の課題です。

--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -1,16 +1,54 @@
-{{-- layouts/profile.blade.phpを読み込む --}}
 @extends('layouts.profile')
+@section('title', 'プロフィールの編集')
 
-
-{{-- admin.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
-@section('title', 'エディット画面')
-
-{{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
 @section('content')
    <div class="container">
       <div class="row">
          <div class="col-md-8 mx-auto">
-            <h2>My エディット</h2>
+            <h2>プロフィールの編集</h2>
+            <form action="{{ route('admin.profile.update') }}" method="post">
+
+               @if (count($errors) > 0)
+                  <ul>
+                     @foreach ($errors->all() as $e)
+                        <li>{{ $e }}</li>
+                     @endforeach
+                  </ul>
+               @endif
+               <div class="form-group row">
+                  <label class="col-md-2">氏名</label>
+                  <div class="col-md-10">
+                     <input type="text" class="form-control" name="name" value="{{ old('name') }}">
+                  </div>
+               </div>
+
+               <div class="form-group row">
+                  <label class="col-md-2">性別</label>
+                  <div class="col-md-10">
+                     <input id="gender_male" type="radio" name="gender" value="male" checked>
+                     <label for="gender_male">男性</label>
+                     <input id="gender_female" type="radio" name="gender" value="female">
+                     <label for="gender_female">女性</label>
+                  </div>
+               </div>
+
+               <div class="form-group row">
+                  <label class="col-md-2">趣味</label>
+                  <div class="col-md-10">
+                     <textarea class="form-control" name="hobby" rows="8">{{ old('hobby') }}</textarea>
+                  </div>
+               </div>
+
+               <div class="form-group row">
+                  <label class="col-md-2">自己紹介欄</label>
+                  <div class="col-md-10">
+                     <textarea class="form-control" name="introduction" rows="8">{{ old('introduction') }}</textarea>
+                  </div>
+               </div>
+
+               @csrf
+               <input type="submit" class="btn btn-primary" value="更新">
+            </form>
          </div>
       </div>
    </div>
--- routes/web.php
+++ routes/web.php
@@ -20,18 +20,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::controller(NewsController::class)->prefix('admin')->group(function () {
-    Route::get('news/create', 'add');
-});
-
-Route::controller(AAAController::class)->group(function () {
-    Route::get('AAA/', 'bbb');
-});
-
-Route::controller(ProfileController::class)->prefix('admin')->group(function () {
-    Route::get('profile/create', 'add');
-    Route::get('profile/edit', 'edit');
-});
+// Route::controller(AAAController::class)->group(function () {
+//     Route::get('AAA/', 'bbb');
+// });
 
 Auth::routes();
 
@@ -42,7 +33,9 @@ Route::controller(NewsController::class)->prefix('admin')->name('admin.')->middl
     Route::post('news/create', 'create')->name('news.create');
 });
 
-Route::controller(ProfileController::class)->prefix('admin')->group(function () {
-    Route::get('profile/create', 'add')->middleware('auth');
-    Route::get('profile/edit', 'edit')->middleware('auth');
+Route::controller(ProfileController::class)->prefix('admin')->name('admin.')->middleware('auth')->group(function () {
+    Route::get('profile/create', 'add')->name('profile.add');
+    Route::post('profile/create', 'create')->name('profile.create');
+    Route::get('profile/edit', 'edit')->name('profile.edit');
+    Route::post('profile/edit', 'update')->name('profile.update');
 });
