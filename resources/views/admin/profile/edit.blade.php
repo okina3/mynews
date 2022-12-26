f32dbc1d1dcc81e579a77098751377a8d1e417a3
okina3 <takosos@icloud.com>
2022-12-26 14:33:57 +0900

    【PHP/Laravel】07 ユーザー認証を実装する。の課題です。

--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -1,16 +1,17 @@
-<!DOCTYPE html>
-<html>
+{{-- layouts/profile.blade.phpを読み込む --}}
+@extends('layouts.profile')
 
-<head>
-   <meta charset="utf-8">
-   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-   <title>MyEdit</title>
-</head>
+{{-- admin.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
+@section('title', 'エディット画面')
 
-<body>
-   <h1>edit作成画面</h1>
-</body>
-
-</html>
+{{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('content')
+   <div class="container">
+      <div class="row">
+         <div class="col-md-8 mx-auto">
+            <h2>My エディット</h2>
+         </div>
+      </div>
+   </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\NewsController;
-
-//課題の3-----------------------------------------------------
 use App\Http\Controllers\AAAController;
-
-//課題の4-----------------------------------------------------
 use App\Http\Controllers\Admin\ProfileController;
 
 /*
@@ -28,12 +24,10 @@ Route::controller(NewsController::class)->prefix('admin')->group(function () {
     Route::get('news/create', 'add');
 });
 
-//課題の3-----------------------------------------------------
 Route::controller(AAAController::class)->group(function () {
     Route::get('AAA/', 'bbb');
 });
 
-//課題の4-----------------------------------------------------
 Route::controller(ProfileController::class)->prefix('admin')->group(function () {
     Route::get('profile/create', 'add');
     Route::get('profile/edit', 'edit');
@@ -44,6 +38,12 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 
-Route::controller(NewsController::class)->prefix('admin')->group(function() {
+Route::controller(NewsController::class)->prefix('admin')->group(function () {
     Route::get('news/create', 'add')->middleware('auth');
+});
+
+//課題の２、３-----------------------------------------
+Route::controller(ProfileController::class)->prefix('admin')->group(function () {
+    Route::get('profile/create', 'add')->middleware('auth');
+    Route::get('profile/edit', 'edit')->middleware('auth');
 });
