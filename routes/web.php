3088533f3fb54b01e07bd916a014de216634834b
okina3 <takosos@icloud.com>
2022-12-26 15:49:07 +0900

    課題前のプッシュ

--- routes/web.php
+++ routes/web.php
@@ -37,12 +37,11 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-
-Route::controller(NewsController::class)->prefix('admin')->group(function () {
-    Route::get('news/create', 'add')->middleware('auth');
+Route::controller(NewsController::class)->prefix('admin')->name('admin.')->middleware('auth')->group(function () {
+    Route::get('news/create', 'add')->name('news.add');
+    Route::post('news/create', 'create')->name('news.create');
 });
 
-//課題の２、３-----------------------------------------
 Route::controller(ProfileController::class)->prefix('admin')->group(function () {
     Route::get('profile/create', 'add')->middleware('auth');
     Route::get('profile/edit', 'edit')->middleware('auth');
