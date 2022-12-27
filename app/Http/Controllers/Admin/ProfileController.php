eb0f7df6ce6e367b038909db34acd04b378da9f1
okina3 <takosos@icloud.com>
2022-12-27 16:31:23 +0900

    【PHP/Laravel】09 投稿データを保存しよう。の課題です。

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -1,10 +1,10 @@
 <?php
-//このページが、【PHP/Laravel】03 ControllerとRoutingの関係について理解しよう。の課題です。
 
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Profile;
 
 class ProfileController extends Controller
 {
@@ -14,10 +14,23 @@ class ProfileController extends Controller
         return view('admin.profile.create');
     }
 
-    public function create()
+    public function create(Request $request)
     {
+        $this->validate($request, Profile::$rules);
+
+        $profile = new Profile;
+        $form = $request->all();
+
+        // フォームから送信されてきた_tokenを削除する
+        unset($form['_token']);
+
+        // データベースに保存する
+        $profile->fill($form);
+        $profile->save();
+
         return redirect('admin/profile/create');
     }
+
     public function edit()
     {
         //これは、admiフォルダのprofileフォルダのedit.blade.phpを指している。
---
+++ app/Models/Profile.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Profile extends Model
+{
+    use HasFactory;
+
+    //idは値を変更しないという意味か？
+    protected $guarded = array('id');
+
+    public static $rules = array(
+        'name' => 'required',
+        'gender' => 'required',
+        'hobby' => 'required',
+        'introduction' => 'required',
+    );
+}
